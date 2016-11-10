242dedcd2aa47224f11efe846b8121e12e5d6d89
Marek Waligórski <marek.waligorski@netguru.pl>
2016-11-10 18:19:16 +0100

    Save the images in project dir right away

--- process.rb
+++ process.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
 require 'open-uri'
 require 'json'
+require 'webrick/httputils'
 
 output = {}
 
@@ -14,12 +14,25 @@ fout = File.open('mapping', 'wt')
 File.open('lista.txt', 'rt') do |f|
   while word = f.readline.rstrip
     print "Processing word: '#{word}': "
-    url = "https://www.googleapis.com/customsearch/v1?q=#{word}&imgSize=large&imgType=clipart&num=1&safe=high&searchType=image&#{api_key}"
+    word.force_encoding('binary')
+    esc_word = WEBrick::HTTPUtils.escape(word)
+    url = "https://www.googleapis.com/customsearch/v1?q=#{esc_word}&imgSize=large&imgType=clipart&num=1&safe=high&searchType=image&hl=pl&lr=lang_pl&#{api_key}"
     res = open(url).read
     link = JSON.load(res)['items'].first['link']
     puts link
     output[word] = link
     fout.puts "#{word}:#{link}"
+
+    extension = link.match(/https?:\/\/.*\.(bmp|jpg|png|jpeg|gif)\??.*/i)[1]
+
+    # Download
+    File.open("apps/web/assets/images/#{word}.#{extension}", "wb") do |saved_file|
+      open(link, "rb") do |read_file|
+        saved_file.write(read_file.read)
+      end
+    end
+
     sleep 2
+
   end
 end
