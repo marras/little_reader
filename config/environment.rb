aadd163f6181bfb0db1f477cdbf4b981572b1944
Marek Waligórski <marek.waligorski@netguru.pl>
2016-11-12 11:07:11 +0100

    Assets

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,14 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'hanami/setup'
-require_relative '../lib/little_reader'
+require 'hanami/assets'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
   mount Web::Application, at: '/'
+end
+
+Hanami::Assets.configure do
+  compile true
+  sources << ['assets']
 end
